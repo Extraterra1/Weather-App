b35a419bda740ce03a624250247cff2fc99f6a64
Carlos Pires <extraterra1@gmail.com>
2023-09-27 15:25:12 +0100

    feat: add search city button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import getWeatherData from './modules/getWeatherData';
 import moment from 'moment/moment';
+import Swal from 'sweetalert2';
 
 const getNewCity = async (evt, city = 'caracas') => {
   const res = await getWeatherData(city);
